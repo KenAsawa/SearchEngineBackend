f9fc769920eb0aeba80882c2b817146fa314ef9e
Nikhil Gupta <nxg160630@utdallas.edu>
2019-11-24 20:16:57 -0600

    remove timing code

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from timeit import default_timer as timer
-
 from circular_shifts import circular_shift
 from scraper import scrape_url
 
 
 def main():
     url = "https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python"
-    start = timer()
     scraped_text = scrape_url(url)
-    end = timer()
-    print("Total time taken to scrape text:", end - start, "seconds")
     shifts_list = []
-    start = timer()
-    circular_shift(scraped_text, url, shifts_list)
-    end = timer()
-    print("Total time taken to shift and merge:", end - start, "seconds")
+    circular_shift(scraped_text, url, shifts_list, "Extracting text from HTML file using Python")
 
 
 main()
